d5714564fe421c3c74aafc90a18fd246181987e0
sargis.khocharyan <sargis.khocharyan@DESKTOP-VN7Q25S>
2023-06-06 18:53:46 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,133 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte t1 = 255;
+            byte t2 = 255;
+            Console.WriteLine(t1 + t2);
+            Console.WriteLine(t1 - t2);
+            Console.WriteLine(t1 * t2);
+            Console.WriteLine(t1 / t2);
+            Console.WriteLine(t1 % t2);
+            Console.WriteLine("-------------");
+
+            sbyte sb1 = 125;
+            sbyte sb2 = 125;
+            Console.WriteLine(sb1 + sb2);
+            Console.WriteLine(sb1 - sb2);
+            Console.WriteLine(sb1 * sb2);
+            Console.WriteLine(sb1 / sb2);
+            Console.WriteLine(sb1 % sb2);
+            Console.WriteLine("-------------");
+
+            short s1 = 788;
+            short s2 = 655;
+            Console.WriteLine(s1 + s2);
+            Console.WriteLine(s1 - s2);
+            Console.WriteLine(s1 * s2);
+            Console.WriteLine(s1 / s2);
+            Console.WriteLine(s1 % s2);
+            Console.WriteLine("-------------");
+
+            ushort u1 = 8689;
+            ushort u2 = 5412;
+            Console.WriteLine(u1 + u2);
+            Console.WriteLine(u1 - u2);
+            Console.WriteLine(u1 * u2);
+            Console.WriteLine(u1 / u2);
+            Console.WriteLine(u1 % u2);
+            Console.WriteLine("-------------");
+
+            long l1 = 46465;
+            long l2 = 6465682;
+            Console.WriteLine(l1 + l2);
+            Console.WriteLine(l1 - l2);
+            Console.WriteLine(l1 * l2);
+            Console.WriteLine(l1 / l2);
+            Console.WriteLine(l1 % l2);
+            Console.WriteLine("-------------");
+
+            ulong ul1 = 564987;
+            ulong ul2 = 564516;
+            Console.WriteLine(ul1 + ul2);
+            Console.WriteLine(ul1 - ul2);
+            Console.WriteLine(ul1 * ul2);
+            Console.WriteLine(ul1 / ul2);
+            Console.WriteLine(ul1 % ul2);
+            Console.WriteLine("-------------");
+
+            int y1 = 80000045;
+            int y2 = 80000045;
+            Console.WriteLine(y1 + y2);
+            Console.WriteLine(y1 - y2);
+            Console.WriteLine(y1 * y2);
+            Console.WriteLine(y1 / y2);
+            Console.WriteLine(y1 % y2);
+            Console.WriteLine("-------------");
+
+            uint j1 = 58942;
+            uint j2 = 23654;
+            Console.WriteLine(j1 + j2);
+            Console.WriteLine(j1 - j2);
+            Console.WriteLine(j1 * j2);
+            Console.WriteLine(j1 / j2);
+            Console.WriteLine(j1 % j2);
+            Console.WriteLine("-------------");
+
+            float f1 = 64465f;
+            float f2 = 654565f;
+            Console.WriteLine(f1 + f2);
+            Console.WriteLine(f1 - f2);
+            Console.WriteLine(f1 * f2);
+            Console.WriteLine(f1 / f2);
+            Console.WriteLine(f1 % f2);
+            Console.WriteLine("-------------");
+
+            double d1 = 95825;
+            double d2 = 89898;
+            Console.WriteLine(d1 + d2);
+            Console.WriteLine(d1 - d2);
+            Console.WriteLine(d1 * d2);
+            Console.WriteLine(d1 / d2);
+            Console.WriteLine(d1 % d2);
+            Console.WriteLine("-------------");
+
+            decimal k1 = 565646m;
+            decimal k2 = 2664565m;
+            Console.WriteLine(k1 + k2);
+            Console.WriteLine(k1 - k2);
+            Console.WriteLine(k1 * k2);
+            Console.WriteLine(k1 / k2);
+            Console.WriteLine(k1 % k2);
+            Console.WriteLine("-------------");
+
+            string str1 = "safhfrsfbhkfsn";
+            string str2 = "jwfahuifbifbjhiw";
+            Console.WriteLine(str1 + str2);
+            Console.WriteLine("-------------");
+
+            char c = 'a';
+            char d = 'g';
+            Console.WriteLine(c + d);
+            Console.WriteLine("-------------");
+
+            Console.Write("X= ");
+            int z = Console.Read();
+            Console.ReadLine();
+            Console.Write("Y= ");
+            int i = Console.Read();
+
+            if (z > i)
+            {
+                Console.WriteLine("X > Y");
+            }
+            else if (z < i)
+            {
+                Console.WriteLine("X < Y");
+            }
+            else Console.WriteLine("X = Y");
+        }
+    }
+}
---
+++ Lesson 2/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+namespace Lesson_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x = 8;
+            //Console.WriteLine(x);
+            //x++;//x=x+1
+            //Console.WriteLine(x);
+            //++x;
+            //Console.WriteLine(x);
+
+            //int a = 64;
+            //int b = 2048;
+
+            //if (a > b)
+            //{
+            //    a = a + b;
+            //}
+            //else
+            //{
+            //    b = b - a;
+            //}
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //int t = a > b ? a = a + b:b = b - a;
+            //Console.WriteLine(t);
+
+            //string s = "Barev";
+            //string g = s.Replace('a', '0');
+            //Console.WriteLine(g);
+            //Console.WriteLine(s);
+
+            //StringBuilder sb = null;
+            //sb.Append("Barev");
+
+            //int                  t  =  new int --> value
+            //StringBuilder sb1 = new StringBuilder("Hello World", 25);
+
+            //StringBuilder sb2 = new StringBuilder("Shat vat or", 25);
+            //Console.WriteLine(sb1.Append(sb2));
+
+            const int a = 2;const int b=3;const int c=4;const int d=5;
+            //if (a > b)
+            //{
+            //    Console.WriteLine(a);
+            //}
+            //else
+            //{
+            //    if (a > c)
+            //    { 
+            //    Console.WriteLine(a);
+            //    }
+            //    else
+            //    {
+            //        if (a > d)
+            //        { 
+            //        Console.WriteLine(a);
+            //        }
+            //        else { Console.WriteLine("a saxic poqra");
+            //        }
+            //    }
+            //}
+            switch (a)
+            {
+                case  b:
+                    Console.WriteLine(a);
+                    break;
+                case   c:
+                    Console.WriteLine(a);
+                    break;
+                case  d:
+                    Console.WriteLine(a);
+                    break;
+                default: Console.WriteLine("a poqra saxic");
+                    break;
+            }
+
+            // Dynamic Type
+            dynamic t1 = "fhiasfh";
+            dynamic t2 = 44564;
+            int oo = t1 + t2;
+
+            string t3 = "safggfu";
+            int t4 = 26464;
+            //int ooo = t3 / t4;
+
+            var uuu = 6745;
+            string yull = "wfjgjhfsbj";
+            //int tg = uuu / yull;
+
+
+
+            int t = 65646464;
+            short v = Convert.ToInt16(t);
+         
+        }
+    }
+}
